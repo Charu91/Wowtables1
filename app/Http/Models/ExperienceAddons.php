16be966bb3c9663996698dec5cd3163158818f03
Srinivas Iyer <srinivasiyer87@gmail.com>
2015-03-17 10:54:20 +0530

    Added new experience models

---
+++ app/Http/Models/ExperienceAddons.php
@@ -0,0 +1,9 @@
+<?php namespace WowTables\Http\Models;
+
+
+class ExperienceAddons extends Experience{
+
+    public function create($product_id, $addons){
+
+    }
+} 
---
+++ app/Http/Models/ExperienceLocation.php
@@ -0,0 +1,20 @@
+<?php namespace WowTables\Http\Models;
+
+
+class ExperienceLocation {
+
+    public function create($data)
+    {
+
+    }
+
+    public function update($data)
+    {
+
+    }
+
+    public function delete($data)
+    {
+
+    }
+} 
