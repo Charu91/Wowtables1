df66ef2fc490fec18768c7a21caaaf824438d2fc
Rushikesh <tech@gourmetitup.com>
2015-03-20 12:15:36 +0530

    Email footer promotions and listpage sidebars functionality

--- public/javascripts/admin/promotions.js
+++ public/javascripts/admin/promotions.js
@@ -8,7 +8,9 @@
             , $editFlagHolder = $('#editFlagHolder')
             , $editFlagBtn = $('.edit-flag-btn')
             , $deleteFlagBtn = $('.delete-flag-btn')
-            , $deleteCollectionBtn = $('.delete-collection-btn');
+            , $deleteCollectionBtn = $('.delete-collection-btn')
+            , $deleteSidebarBtn = $('.delete-sidebar-btn')
+            , $deleteEmailFooterPromotionBtn = $('.delete-efp-btn');
 
         $editFlagBtn.on('click',function(){
 
@@ -60,6 +62,48 @@
                 $.ajax({
                     method: 'DELETE',
                     url: '/admin/promotions/collections/' + id,
+                    headers: {
+                        'X-XSRF-TOKEN': token
+                    }
+                }).done(function () {
+                    location.reload();
+                }).fail(function (jqXHR) {
+                    console.log(jqXHR);
+                });
+            }
+
+        });
+
+        $deleteSidebarBtn.on('click',function(){
+
+            var id = $(this).data('sidebar-id');
+
+            if(confirm('Are you sure you want to delete sidebar with id '+id+'?'))
+            {
+                $.ajax({
+                    method: 'DELETE',
+                    url: '/admin/promotions/listpage_sidebar/' + id,
+                    headers: {
+                        'X-XSRF-TOKEN': token
+                    }
+                }).done(function () {
+                    location.reload();
+                }).fail(function (jqXHR) {
+                    console.log(jqXHR);
+                });
+            }
+
+        });
+
+        $deleteEmailFooterPromotionBtn.on('click',function(){
+
+            var id = $(this).data('efp-id');
+
+            if(confirm('Are you sure you want to delete footer promotion with id '+id+'?'))
+            {
+                $.ajax({
+                    method: 'DELETE',
+                    url: '/admin/promotions/email_footer_promotions/' + id,
                     headers: {
                         'X-XSRF-TOKEN': token
                     }
