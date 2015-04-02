18cbafe7791df30b20b22e1095a7a027f0d2de93
Parth Shukla <shuklaparth@hotmail.com>
2015-04-02 15:45:32 +0530

    added method foramtLocationFilters

---
+++ config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+//file: config/constants.php
+
+/**
+ * Constants for application.
+ * 
+ * @since	1.0.0
+ * @author	Parth Shukla <shuklaparth@hotmail.com>
+ */
+ 
+ return [
+ 	"IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/'
+ ];
