036eb4a6c40713367db612ba43f3130638fa5dd7
Parth Shukla <shuklaparth@hotmail.com>
2015-04-11 17:08:01 +0530

    git push issue fixing

--- app/Http/Models/ALaCarte.php
+++ app/Http/Models/ALaCarte.php
@@ -25,7 +25,7 @@ use Config;
 	 * @since	1.0.0
 	 * @version	1.0.0
 	 */
-	public static function getALaCarteDetails($aLaCarteID) {
+	public static function getALaCarteDetails( $aLaCarteID ) {
 		
 		//array to store the matching result
 		$arrData = array();
