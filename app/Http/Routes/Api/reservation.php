18e7062ce1690283b2b2f0a85af5838157e94fec
Parth Shukla <shuklaparth@hotmail.com>
2015-04-17 20:56:24 +0530

    added route definition for schedules

--- app/Http/Routes/Api/reservation.php
+++ app/Http/Routes/Api/reservation.php
@@ -5,3 +5,5 @@
  Route::get('api/reservation/location/{type}/{id}','Api\ReservationController@getLocation');
  
  Route::get('api/reservation/party_size/{type}/{id}','Api\ReservationController@getPartySize');
+ 
+ Route::get('api/reservation/schedule/{type}/{id}/{day?}','Api\ReservationController@getSchedule');
