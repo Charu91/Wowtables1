3ba6d2389c2096457bb2956bcdc70ce7960ed261
Parth Shukla <shuklaparth@hotmail.com>
2015-04-23 20:02:02 +0530

    added route definition for reserving table

--- app/Http/Routes/Api/reservation.php
+++ app/Http/Routes/Api/reservation.php
@@ -7,3 +7,5 @@
  Route::get('api/reservation/party_size/{type}/{id}','Api\ReservationController@getPartySize');
  
  Route::get('api/reservation/schedule/{type}/{id}/{day?}','Api\ReservationController@getSchedule');
+ 
+ Route::post('api/reservation/reserve','Api\ReservationController@reserveTable');
