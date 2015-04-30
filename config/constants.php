768aefbc08a0de3849bc327e2fa6eaa88b2871d8
Parth Shukla <shuklaparth@hotmail.com>
2015-04-30 23:22:04 +0530

    added link to gallery and mobile images

--- config/constants.php
+++ config/constants.php
@@ -13,5 +13,7 @@
  	"IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/',
  	"API_ERROR" => 'FAIL',
  	"API_SUCCESS" => 'OK',
- 	"API_LISTING_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/listing/'
+ 	"API_LISTING_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/listing/',
+ 	"API_GALLERY_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/gallery/',
+ 	"API_MOBILE_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/mobile/'
  ];
