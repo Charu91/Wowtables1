2645c3e5d9d2b3e2b44f92949b92e26b921530db
Parth Shukla <shuklaparth@hotmail.com>
2015-05-13 16:58:20 +0530

    added route definition for restaurant search

--- app/Http/Routes/Api/search.php
+++ app/Http/Routes/Api/search.php
@@ -1,3 +1,6 @@
 <?php
 Route::post('/search/vendors', 'Api\SearchController@find');
 Route::get('/search/experience', 'Api\SearchController@searchExperience');
+
+Route::get('api/search/restaurants/{matchString}', 'Api\SearchController@getMatchingRestaurantsName');
+Route::get('api/search/detail/{type}/{id}', 'Api\SearchController@getResourceDetail');
