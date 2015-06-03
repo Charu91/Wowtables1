20cb25011354934ee94e623cdc6fef2356c72f03
Gourmetitup Tech <tech@gourmetitup.com>
2015-06-03 13:51:00 +0530

    experience model

--- app/Http/Models/Experience.php
+++ app/Http/Models/Experience.php
@@ -337,17 +337,15 @@ class Experience extends Product{
     protected function savePricing($productId, $pricing){
         $pricing_insert_data = [
             'product_id' => $productId,
-            'price' => isset($pricing['price']) ? $pricing['price'] : null,
-            'tax' => isset($pricing['tax'])? $pricing['tax'] : null,
-            'post_tax_price' => isset($pricing['post_tax_price'])? $pricing['post_tax_price'] : null,
-            'commission' => isset($pricing['commission'])? $pricing['commission'] : null,
+            'price' => isset($pricing['price']) ? $pricing['price'] : 0,
+            'tax' => isset($pricing['tax'])? $pricing['tax'] : 0,
+            'post_tax_price' => isset($pricing['post_tax_price'])? $pricing['post_tax_price'] : 0,
+            'commission' => isset($pricing['commission'])? $pricing['commission'] : 0,
             'price_type' => $pricing['price_types'],
-            'taxes' => isset($pricing['taxes'])? $pricing['taxes'] : "Inclusive-Taxes",
+            'taxes' => isset($pricing['taxes'])? $pricing['taxes'] : "Taxes Applicable",
+            'commission_on' => isset($pricing['commission_on'])? $pricing['commission_on'] : "Post-Tax",
         ];
 
-        if(isset($data['commission_on'])){
-            $pricing_insert_data['commission_on'] = $pricing['commission_on'];
-        }
 
         $pricingInsert = DB::table('product_pricing')->insert($pricing_insert_data);
 
