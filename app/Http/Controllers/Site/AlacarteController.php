4f6e79309fa5fd6d0b4fb031765ebbb16212cab8
Gourmetitup Tech <tech@gourmetitup.com>
2015-06-15 19:47:13 +0530

    reservation changes

--- app/Http/Controllers/Site/AlacarteController.php
+++ app/Http/Controllers/Site/AlacarteController.php
@@ -386,7 +386,7 @@ class AlacarteController extends Controller {
                         'No_of_People' => $dataPost['partySize'],
                         'Date_of_Visit' => date('d-M-Y', strtotime($dataPost['reservationDate'])),
                         'Time' => date("G:ia", strtotime($dataPost['reservationTime'])),
-                        //'Alternate_ID' =>  'A'.sprintf("%06d",$arrResponse['data']['reservationID']),//sprintf("%06d",$this->data['order_id1']);
+                        'Alternate_ID' =>  'A'.sprintf("%06d",$reservationResponse['data']['reservationID']),//sprintf("%06d",$this->data['order_id1']);
                         'Occasion' => $dataPost['specialRequest'],
                         'Type' => "Alacarte",
                         'API_added' => 'Yes',
