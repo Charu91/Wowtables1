7cb1246e186b2b5a41b1a6c50e97a328446967a0
wowtables <wowtables@wowtables-Latitude-3540.(none)>
2015-06-18 13:31:58 +0530

    remove print r

--- app/Http/Controllers/Site/ExperienceController.php
+++ app/Http/Controllers/Site/ExperienceController.php
@@ -125,8 +125,8 @@ class ExperienceController extends Controller {
                                    'meta_desc'     => $metaDescDetails, 
                                    'meta_keywords' => $meta_keywords);
         
-        print_r($data);
-        exit;
+        /*print_r($data);
+        exit;*/
         return view('frontend.pages.experiencedetails',$data)
                         ->with('meta_information', $meta_information);
     }
