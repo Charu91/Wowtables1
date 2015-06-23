1937d2d4d543f80d2aa41891dc5f3dc6eb77c24d
Gourmetitup Tech <tech@gourmetitup.com>
2015-06-23 13:21:51 +0530

    meta tags changes

--- resources/views/frontend/templates/inner_pages.blade.php
+++ resources/views/frontend/templates/inner_pages.blade.php
@@ -35,8 +35,7 @@ if((Session::get('add_mixpanel_event') != 0 || Session::get('add_mixpanel_event'
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="robots" content="noindex,nofollow" />
-<title>{!! $meta_information['seo_title'] or 'WowTables : Exclusive fine dining meals & experiences in your city' !!}</title>
+<title>{!! $meta_information['seo_title'] or 'WowTables: Exclusive fine dining meals & experiences in your city' !!}</title>
 <meta name="title" content="{!! $seo_title or 'WowTables' !!}">
 <meta name="description" content="{!! $meta_information['meta_desc'] or 'Search, discover, reserve & experience fine dining in Mumbai, Delhi, Bangalore & Pune' !!}">
 <meta name="keywords" content="{!! $meta_information['meta_keywords'] or 'WowTables' !!}">    
