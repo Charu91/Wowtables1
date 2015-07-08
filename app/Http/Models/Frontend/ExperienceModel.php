8107fcfa96ad8f38697ce067cd934374fe1eb227
Gourmetitup Tech <tech@gourmetitup.com>
2015-07-08 20:44:51 +0530

    query changes

--- app/Http/Models/Frontend/ExperienceModel.php
+++ app/Http/Models/Frontend/ExperienceModel.php
@@ -166,13 +166,13 @@ class ExperienceModel {
                 ->where('products.visible',1)
                 ->whereIN('products.type',array('simple','complex'))
                 ->groupBy('products.id')
-                ->orderBy('pvl.order_status')
+                ->orderBy('pvl.order_status','asc')
                 ->select('products.id','products.name as title','pat.attribute_value as description',
                       'pat2.attribute_value as short_description', 'pp.price', 'pt.type_name as price_type',
                       'pp.is_variable', 'pp.tax', 'pp.post_tax_price', 'media.file as image', 
                       'products.type as product_type', 'flags.name as flag_name','flags.color as flag_color', 'locations.id as location_id', 
                       'locations.name as location_name','products.slug');
-        
+
       //adding filter for cuisines if cuisines are present
       if(isset($arrData['cuisine']) && !empty($arrData['cuisine'])) {
         $experienceQuery->join(DB::raw('product_attributes_multiselect as pam'),'pam.product_id','=','products.id')
