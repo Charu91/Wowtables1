b2ca0593706751ae34f61c8d764405d8b0098558
Parth Shukla <parthshukla@ahex.co.in>
2015-07-17 19:16:26 +0530

    Collection listing code modified

--- app/Http/Models/CollectionTags.php
+++ app/Http/Models/CollectionTags.php
@@ -24,15 +24,44 @@ class CollectionTags {
 
         //echo "Hi...."; 
         	$queryResult=DB::table('tags as t')        	 		
-        	 		->leftjoin('media_resized_new as mrn','t.media_id','=','mrn.id')
-        	 		->where('status','=','available')
+        	 		//->leftjoin('media_resized_new as mrn','t.media_id','=','mrn.id')
+        	 		->leftJoin('media_resized_new as mrn1', function($join) {
+                                                $join->on('mrn1.media_id', '=', 't.media_id')
+                                                     ->where('mrn1.image_type', '=' , 'mobile_listing_ios_experience');
+                        })
+                    ->leftJoin('media_resized_new as mrn2', function($join) {
+                                                $join->on('mrn2.media_id', '=', 't.media_id')
+                                                     ->where('mrn2.image_type', '=', 'mobile_listing_android_experience');
+                        })
+                    ->where('status','=','available')
         	 		->select('t.id','t.name','t.status','t.description',
-        	 				  DB::Raw('IFNULL(mrn.file,"") AS file')
-        	 				)
+        	 				  //DB::Raw('IFNULL(mrn.file,"") AS file',
+                            'mrn1.file as ios_image','mrn2.file as android_image'                                
+        	 				)   
         	 		->get();
 
-        	 		$arrResponse['data']=$queryResult;        	 		
-        	 		$arrResponse['status'] = Config::get('constants.API_SUCCESS');
+                    if($queryResult) {
+                        foreach($queryResult as $row) {
+                            $data[] = array(
+                                                            'id'         => $row->id,
+                                                            'name'          => $row->name,
+                                                            'status'       => (empty($row->status)) ? "" : $row->status,
+                                                            'description' => (empty($row->description)) ? "" : $row->description,                                                            
+                                                            'image' => array(
+                                                                                'mobile_listing_ios_experience' => (empty($row->ios_image))? "":Config::get('constants.API_MOBILE_IMAGE_URL').$row->ios_image,
+                                                                                'mobile_listing_android_experience' => (empty($row->android_image))? "":Config::get('constants.API_MOBILE_IMAGE_URL').$row->android_image,
+                                                                             )
+                                                        );
+                        }
+                    }
+                    else {
+                        $data[] = array();
+                    }
+
+                            
+            $arrResponse['data']=$data;      
+        	//$arrResponse['data']=$queryResult;        	 		
+        	$arrResponse['status'] = Config::get('constants.API_SUCCESS');
             
             return $arrResponse;
         }
