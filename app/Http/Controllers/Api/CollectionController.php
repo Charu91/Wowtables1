1e7b755d411d3203b4809134bd996587cd143cff
Parth Shukla <shuklaparth@hotmail.com>
2015-07-20 11:43:21 +0530

    Updated code for final response.

--- app/Http/Controllers/Api/CollectionController.php
+++ app/Http/Controllers/Api/CollectionController.php
@@ -49,60 +49,21 @@ class CollectionController extends Controller {
 						);
 		//reading the alacarte details 
 		$this->listings->fetchListings($filters);
-        $arrResult = $this->listings->arr_result;
+        $arrAlacarte = $this->listings->arr_result;
 
+        //reading details of the experiences
+        $arrExperiences = $this->experienceList->findMatchingExperience($filters);
 
-		if(empty($arrResult)) {
-			$arrResult['status'] = Config::get('constants.API_SUCCESS');
-			$arrResult['no_result_msg'] = 'No matching data found.';
-			$arrResult['data'] = array(
-										'listing' => array()
-									);
-			$arrResult['total_count'] = 0;
-			}
-			else {
-			$arrResult = array(
-							'status' => Config::get('constants.API_SUCCESS'),
-							'data' => array(
-											'listing' => $arrResult,
-            								//'filters' => $this->listings->filters,            								
-            								'total_pages' => $this->listings->total_pages,
-            								'sort_options' => $this->listings->sort_options,
-            							),
-            				'total_count' => $this->listings->total_count,
-            				'no_result_msg' => 'No matching result found.'
-										
-						);
-			}	
+        $arrResponse['status'] = Config::get('constants.API_SUCCESS');
 
-        	$searchResult= $this->experienceList->findMatchingExperience($filters);
-        			
-				if(!empty($searchResult)) {
-					//setting up the array to be formatted as json
-					$arrResult['status2'] = Config::get('constants.API_SUCCESS');
-					$arrResult['resultCount2'] = $searchResult['resultCount'];
-					$arrResult['data2'] = (array_key_exists('data', $searchResult)) ? $searchResult['data']:array();
-					//$arrResult['filters2'] = $this->experienceList->getExperienceSearchFilters();
-					$arrResult['no_result_msg2'] = 'No matching data found.';
-				}
-				else {
-					$arrResult['status2'] = Config::get('constants.API_SUCCESS');
-					$arrResult['resultCount2'] = 0;
-					$arrResult['data2'] = array();
-					$arrResult['no_result_msg2'] = 'No matching data found.';
-				}
+		$arrResponse['data'] = CollectionTags::getCollectionTagDetail($tagID);
+		$arrResponse['data']['alacarte'] = $arrAlacarte;
+		$arrResponse['data']['alacarteResultCount']  = count($arrAlacarte);
+		$arrResponse['data']['experience'] = $arrExperiences;
+		$arrResponse['data']['experienceResultCount'] = count($arrExperiences);;				
+		$arrResponse['no_result_msg'] = 'No matching data found.';
 
-
-				$arrResponse['status'] = Config::get('constants.API_SUCCESS');
-
-				$arrResponse['data'] = CollectionTags::getCollectionTagDetail($tagID);
-				$arrResponse['data']['alacarte'] = $arrResult['data1']['listing'];
-				$arrResponse['data']['alacarteResultCount']  = $arrResult['total_count1'];
-				$arrResponse['data']['experience'] = $arrResult['data2'];
-				$arrResponse['data']['experienceResultCount'] = $arrResult['resultCount2'];				
-				$arrResponse['no_result_msg'] = 'No matching data found.';
-
-				return response()->json($arrResponse,200);      	
+		return response()->json($arrResponse,200);      	
 			
 	}
 }
