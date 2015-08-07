a74251bf91f7c4184bd01a205b51ebd021b8fbbb
Gourmetitup Tech <tech@gourmetitup.com>
2015-08-07 19:37:30 +0530

    fb login

--- app/Http/Models/Facebook.php
+++ app/Http/Models/Facebook.php
@@ -132,7 +132,7 @@ class Facebook {
             if($user[0]->fb_token_exists) { 
 
                 if($user[0]->location_id) {
-                    $location_slug = DB::table('locations')->where('id', $user[0]->location_id)->pluck('slug');
+                    $location_slug = DB::table('locations')->where('id', $user[0]->location_id)->pluck('name');
 
                     if($location_slug){
                         $retarr = ['status' => 'success', 'location' => $location_slug];
@@ -151,7 +151,7 @@ class Facebook {
                 ]);
 
                 if($user[0]->location_id){
-                    $location_slug = DB::table('locations')->where('id', $user[0]->location_id)->pluck('slug');
+                    $location_slug = DB::table('locations')->where('id', $user[0]->location_id)->pluck('name');
 
                     if($location_slug){
                         $retarr = ['status' => 'success', 'location' => $location_slug];
