230ae6ba4799a937b3cf8b146df8f0d31f46ccfe
Gourmetitup Tech <tech@gourmetitup.com>
2015-08-11 12:55:47 +0530

    total amount change

--- resources/views/site/pages/payment.blade.php
+++ resources/views/site/pages/payment.blade.php
@@ -69,8 +69,8 @@ $action_url = "https://secure.payu.in/_payment";
     <!--<input name="surl" type="hidden"  value="http://wowtables.app/payment/process_response" />
     <input name="furl" type="text"  value="http://wowtables.app/payment/process_response" />-->
 
-     <input name="surl" type="hidden"  value="http://wowtables.com/payment/process_response" />
-    <input name="furl" type="text"  value="http://wowtables.com/payment/process_response" />
+     <input name="surl" type="hidden"  value="http://dev.wowtables.com/payment/process_response" />
+    <input name="furl" type="text"  value="http://dev.wowtables.com/payment/process_response" />
     <input name="api_version" type="hidden"   value="1" />
     <input name="hash" type="hidden" size="60" value="<?php echo $secure_hash;?>" />
 
