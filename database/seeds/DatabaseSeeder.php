dada50ee172e0cefe79dbe90582f095837150253
mannan <manan@wowtables.com>
2015-09-28 15:50:51 +0530

    seeder file changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
 		Model::unguard();
 
 		// $this->call('UserTableSeeder');
-		//$this->call('ReservationAttributesSeeder');
+		$this->call('ReservationAttributesSeeder');
 		$this->call('ReservationStatusesSeeder');
 	}
 }
