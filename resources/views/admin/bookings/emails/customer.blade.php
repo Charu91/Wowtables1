ec3c3a31be1b6dcb8b09763cd35b3e22bdbd7fb8
mannan <manan@wowtables.com>
2015-09-29 15:17:10 +0530

    change in email format going to customer.

--- resources/views/admin/bookings/emails/customer.blade.php
+++ resources/views/admin/bookings/emails/customer.blade.php
@@ -10,6 +10,9 @@
     <tr>
         <td>Your reservation for the WowTables experience at {{ $data['outlet'] }} is confirmed for {{ $data['date'] }} at {{ $data['time'] }} for {{ $data['no_of_people'] }} guests</td>
     </tr>
+    <tr>
+        <td>We hope you enjoy your experience. You can change or cancel reservations upto 2 hours in advance by visitingwowtables.com/myreservations. For last minute changes please call our concierge desk at 9619551387 or the restaurant directly. Note that certain reservations cannot be changed or cancelled.</td>
+    </tr>
     <tr><td>&nbsp;</td></tr>
     <tr>
         <td>Regards,</td>
