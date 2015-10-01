e43c7e25d072db074eec9a48194e8a8213ae3352
mannan <manan@wowtables.com>
2015-10-01 12:39:01 +0530

    product review error fix.

---
+++ app/Http/Models/ProductReviews.php
@@ -0,0 +1,6 @@
+<?php namespace WowTables\Http\Models;
+
+
+class ProductReviews {
+    protected $table = 'product_reviews';
+} 
---
+++ app/Http/Models/VendorLocationsReviews.php
@@ -0,0 +1,6 @@
+<?php namespace WowTables\Http\Models;
+
+
+class VendorLocationsReviews {
+    protected $table = 'vendor_location_reviews';
+} 
