7e33a9e02bcd7cd792f593d57be29a4a7cac88e6
mannan <manan@wowtables.com>
2015-10-08 14:54:05 +0530

    push notification test 2

--- app/Http/Models/Eloquent/Reservations/ReservationDetails.php
+++ app/Http/Models/Eloquent/Reservations/ReservationDetails.php
@@ -524,13 +524,15 @@ class ReservationDetails extends Model {
         $tokens = array();
         //print_r($vendorUsers);die;
         foreach($vendorUsers as $vendorUser){
-            echo $vendorUser->user_id."<br/>";
+            //echo $vendorUser->user_id."<br/>";
             $userDevice = DB::table('user_devices')->where('user_id',$vendorUser->user_id)->first();
             //print_r($userDevice);
             //echo $userDevice->notification_id;
-            $tokens[] = $userDevice->notification_id;
+            if(isset($userDevice->notification_id)) {
+                $tokens[] = $userDevice->notification_id;
+            }
         }
-        //print_r($tokens);
+        print_r($tokens);
         die;
         /*$ch = curl_init();
         $curlConfig = array(
