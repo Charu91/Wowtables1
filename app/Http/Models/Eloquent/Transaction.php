b674d458689567fc7a4db7d2806a035b5d59a8aa
Parth Shukla <parthshukla@ahex.co.in>
2015-10-16 13:35:11 +0530

    Added Model class Transaction.

---
+++ app/Http/Models/Eloquent/Transaction.php
@@ -0,0 +1,49 @@
+<?php namespace WowTables\Http\Models\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Transaction class.
+ *
+ * @since   1.0.0
+ * @version 1.0.0
+ * @author  Parth Shukla <parthshukla@ahex.co.in>
+ */
+class Transaction extends Model {
+
+	/**
+	 * Rules for validating a transaction.
+	 *
+	 * @access  public
+	 * @param   array
+	 * @since   1.0.0
+	 */
+	public static $arrRules = array(
+						'transaction_number'	=> 'required',
+						'reservation_id'		=>  'required|exists:reservation_details,id',
+						'amount_paid'			=> 'required',
+						'transaction_date'		=> 'required|date|date_format:Y-m-d',
+						'transaction_details'	=> 'required|2048',
+						'response_code'			=> 'required|max:255',
+						'response_message'		=> 'required|max:2048',
+						'source_type'			=>  'required'
+					);
+
+	/**
+     * The database table used by the model.
+     *
+     * @access  protected
+     * @var     string
+     */
+    protected $table = 'transactions';
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @access   protected
+     * @var      array
+     */
+    protected $guarded = array('id');
+}
+//end of class Transaction
+// end of file Transaction.php
