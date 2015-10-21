72accb7d0a494ac0436442cbb04c041c37b672ba
Biju Chandran <biju@binaryveda.com>
2015-10-21 18:37:22 +0530

    Added a new migration for rest_access_token

--- database/migrations/2015_10_21_183010_add_resttoken_to_devices_table.php
+++ database/migrations/2015_10_21_183010_add_resttoken_to_devices_table.php
@@ -16,6 +16,7 @@ class AddResttokenToDevicesTable extends Migration {
 		{
 			$table->string('rest_access_token')->after('access_token');
 			$table->timestamp('rest_access_token_expires')->after('access_token_expires');
+			$table->string('rest_notification_id')->after('notification_id');
 		});
 	}
 
@@ -30,6 +31,7 @@ class AddResttokenToDevicesTable extends Migration {
 		{
 			$table->dropColumn('rest_access_token');
 			$table->dropColumn('rest_access_token_expires');
+			$table->dropColumn('rest_notification_id');
 		});
 	}
 
