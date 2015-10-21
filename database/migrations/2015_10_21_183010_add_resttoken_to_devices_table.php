09e08621a0d3fbd5da703b8d17f7e23bee55190a
Biju Chandran <biju@binaryveda.com>
2015-10-21 19:06:18 +0530

    Changed REST column migration to default to NULL

--- database/migrations/2015_10_21_183010_add_resttoken_to_devices_table.php
+++ database/migrations/2015_10_21_183010_add_resttoken_to_devices_table.php
@@ -14,9 +14,9 @@ class AddResttokenToDevicesTable extends Migration {
 	{
 		Schema::table('user_devices', function(Blueprint $table)
 		{
-			$table->string('rest_access_token')->after('access_token');
-			$table->timestamp('rest_access_token_expires')->after('access_token_expires');
-			$table->string('rest_notification_id')->after('notification_id');
+			$table->string('rest_access_token')->after('access_token')->nullable()->default(null);
+			$table->timestamp('rest_access_token_expires')->after('access_token_expires')->nullable()->default(null);
+			$table->string('rest_notification_id')->after('notification_id')->nullable()->default(null);
 		});
 	}
 
