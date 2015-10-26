ffbf7cd8a24766d182e11987408fe8f4fdb10a87
Parth Shukla <parthshukla@ahex.co.in>
2015-10-26 20:13:04 +0530

    Updated code for initializing different hashes.

--- app/Http/Models/Eloquent/ReservationDetails.php
+++ app/Http/Models/Eloquent/ReservationDetails.php
@@ -363,7 +363,16 @@ class ReservationDetails extends Model {
 										'shortDescription'	=> $productDetail['descriptive_title'],
 										'email'				=> $arrData['guestEmail'],
 									);
-					$arrResponse['data']['hash'] = Payment::getPayUHash($arrPayUData);
+
+					$arrHash = Payment::getPayUHash($arrPayUData);
+
+					//setting up the hash value
+					$arrResponse['data']['hash'] = array(
+														'paymentHash' 		=> $arrHash['paymentHash'],
+														'paymentRelatedDetailsForMobileSDKHash' => (array_key_exists('paymentRelatedDetailsForMobileSDKHash', $arrHash)) ? $arrHash['paymentRelatedDetailsForMobileSDKHash']:'',
+														'getUserCardsHash'	=> (array_key_exists('getUserCardsHash', $arrHash)) ? $arrHash['getUserCardsHash'] : '',
+														'saveUserCardHash'	=> (array_key_exists('saveUserCardHash', $arrHash)) ? $arrHash['saveUserCardHash'] : ''																							 
+													); 
 
 				}
 			}
