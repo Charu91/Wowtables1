4795dba3f81feb58b7d53259639e208169c52e78
mannan <manan@wowtables.com>
2015-10-27 17:42:48 +0530

    fixed menu design

--- app/Http/Models/Experience.php
+++ app/Http/Models/Experience.php
@@ -116,8 +116,8 @@ class Experience extends Product{
                                     $attribute_inserts[$typeTableAliasMap[$attributesMap[$attribute]['type']]['table']][] = [
                                         'product_id' => $productId,
                                         'product_attribute_id' => $attributeIdMap[$attribute],
-                                        //'attribute_value' => $this->parseMenu($value)
-                                        'attribute_value' => json_encode($value)
+                                        'attribute_value' => $this->parseMenu($value)
+                                        //'attribute_value' => json_encode($value)
                                     ];
                                 }else{
                                     if($value != "" || $value != " ") {
