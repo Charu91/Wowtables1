a210a15b3078bb14fbd55a3258e80698761737cc
Biju Chandran <biju@binaryveda.com>
2015-10-30 20:05:41 +0530

    Testing

--- app/Http/Routes/ConciergeApi/reservation.php
+++ app/Http/Routes/ConciergeApi/reservation.php
@@ -6,18 +6,18 @@ Route::get('conciergeapi/reservation/{statuses}/{location}', [
 ]);
 //Get a particular reservation for edit
 Route::get('conciergeapi/reservation/{id}/edit', [
-    'uses' => 'ConciergeAPI\ReservationController@edit',
-    'middleware' => 'concierge.api'
+    'uses' => 'ConciergeAPI\ReservationController@edit'//,
+    //'middleware' => 'concierge.api'
 ]);
 //Get a particular reservation for display
 Route::get('conciergeapi/reservation/{id}', [
-    'uses' => 'ConciergeAPI\ReservationController@show',
-    'middleware' => 'concierge.api'
+    'uses' => 'ConciergeAPI\ReservationController@show'//,
+    //'middleware' => 'concierge.api'
 ]);
 //Update a particular reservation
 Route::put('conciergeapi/reservation/{id}', [
-    'uses' => 'ConciergeAPI\ReservationController@update',
-    'middleware' => 'concierge.api'
+    'uses' => 'ConciergeAPI\ReservationController@update'//,
+    //'middleware' => 'concierge.api'
 ]);
 
 //Update a particular reservation
