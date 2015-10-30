bbbac7af6295fc95b6fb4c2dde0d9850d4272048
Biju Chandran <biju@binaryveda.com>
2015-10-30 20:19:38 +0530

    Changed get method to accept rest_access-token

--- app/Http/Routes/ConciergeApi/reservation.php
+++ app/Http/Routes/ConciergeApi/reservation.php
@@ -1,28 +1,28 @@
 <?php
 //Get Reservations based on status
 Route::get('conciergeapi/reservation/{statuses}/{location}', [
-    'uses' => 'ConciergeAPI\ReservationController@index'//,
-    //'middleware' => 'concierge.api'
+    'uses' => 'ConciergeAPI\ReservationController@index',
+    'middleware' => 'concierge.api'
 ]);
 //Get a particular reservation for edit
 Route::get('conciergeapi/reservation/{id}/edit', [
-    'uses' => 'ConciergeAPI\ReservationController@edit'//,
-    //'middleware' => 'concierge.api'
+    'uses' => 'ConciergeAPI\ReservationController@edit',
+    'middleware' => 'concierge.api'
 ]);
 //Get a particular reservation for display
 Route::get('conciergeapi/reservation/{id}', [
-    'uses' => 'ConciergeAPI\ReservationController@show'//,
-    //'middleware' => 'concierge.api'
+    'uses' => 'ConciergeAPI\ReservationController@show',
+    'middleware' => 'concierge.api'
 ]);
 //Update a particular reservation
 Route::put('conciergeapi/reservation/{id}', [
-    'uses' => 'ConciergeAPI\ReservationController@update'//,
-    //'middleware' => 'concierge.api'
+    'uses' => 'ConciergeAPI\ReservationController@update',
+    'middleware' => 'concierge.api'
 ]);
 
 //Update a particular reservation
 Route::get('conciergeapi/experience/{id}', [
-    'uses' => 'ConciergeAPI\ReservationController@getExperienceDetails'//,
-    //'middleware' => 'concierge.api'
+    'uses' => 'ConciergeAPI\ReservationController@getExperienceDetails',
+    'middleware' => 'concierge.api'
 ]);
 
