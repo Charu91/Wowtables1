80b592a9e96af18577d3bd28fc2c2e9cf421a350
mannan <manan@wowtables.com>
2015-11-02 12:24:56 +0530

    manual merge conflict fix

--- config/constants.php
+++ config/constants.php
@@ -4,31 +4,30 @@
 
 /**
  * Constants for application.
- * 
+ *
  * @since	1.0.0
  * @author	Parth Shukla <shuklaparth@hotmail.com>
  */
- 
- return [
- 	"IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/',
- 	"API_ERROR" => 'FAIL',
- 	"API_SUCCESS" => 'OK',
- 	"API_LISTING_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/listing/',
- 	"REGISTRATION_PAGE_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/registration/',
- 	"API_GALLERY_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/gallery/',
- 	"API_MOBILE_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/mobile/',
- 	"LISTPAGE_SIDEBAR_WEB_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/sidebars/',
- 	"NEXT_RESERVATION_TIME_RANGE_LIMIT" => 2, //hours after or before an existing reservation, next reservation is allowed to a user for same date
- 	"SERVER_TIME_CUTOFF_FOR_RESERVATION" => '20:30:00', //time upto which server will accept the reservation requests
- 	"MIN_SUPPORTED_IOS_VERSION" => '1.1.0',
- 	"MIN_SUPPORTED_ANDROID_VERSION" => '1.0.9',
-	 "MIN_SUPPORTED_ANDROID_VERSION_CONCIERGE" => '1.1.0',
- 	"API_UPDATE" => 'Update',
- 	"API_NEARBY_DISTANCE" => 15,
+
+return [
+	"IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/',
+	"API_ERROR" => 'FAIL',
+	"API_SUCCESS" => 'OK',
+	"API_LISTING_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/listing/',
+	"REGISTRATION_PAGE_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/registration/',
+	"API_GALLERY_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/gallery/',
+	"API_MOBILE_IMAGE_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/mobile/',
+	"LISTPAGE_SIDEBAR_WEB_URL" => 'https://s3-eu-west-1.amazonaws.com/wowtables/uploads/sidebars/',
+	"NEXT_RESERVATION_TIME_RANGE_LIMIT" => 2, //hours after or before an existing reservation, next reservation is allowed to a user for same date
+	"SERVER_TIME_CUTOFF_FOR_RESERVATION" => '20:30:00', //time upto which server will accept the reservation requests
+	"MIN_SUPPORTED_IOS_VERSION" => '1.1.0',
+	"MIN_SUPPORTED_ANDROID_VERSION" => '1.1.7',
+	"API_UPDATE" => 'Update',
+	"API_NEARBY_DISTANCE" => 15,
 
 
 
- 	/* DETAILS for PAYU to be used for testing purpose */
- 	"PAYU_MERCHANT_ID" => 'gtKFFX',
- 	"PAYU_SALT"        => 'eCwWELxi',
- ];
+	/* DETAILS for PAYU to be used for testing purpose */
+	"PAYU_MERCHANT_ID" => '0MQaQP',//'gtKFFX',
+	"PAYU_SALT"        => '13p0PXZk',//'eCwWELxi',
+];
