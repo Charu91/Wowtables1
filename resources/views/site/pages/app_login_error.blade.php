e905757d1d0ff9558ba936e0d912b05a0aab9a3a
vineet-devzila <vineet@devzila.com>
2015-11-17 19:52:07 +0530

    Sending Mail on App Login Error

---
+++ resources/views/site/pages/app_login_error.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>App Login Issue</title>
+</head>
+
+<body>
+
+<p>User has found error while surfing the <?php echo $data['login_type'];?>. Below are the few details:</p>
+<p> User Email: <?php echo $data['email'];?></p>
+<p> Message: <?php echo $data['message'];?></p>
+<p> Action: <?php echo $data['action'];?></p>
+<p> Code: <?php echo $data['code'];?></p>
+
+</body>
+</html>
